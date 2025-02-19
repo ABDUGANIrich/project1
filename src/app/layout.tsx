283631db6878ccc12639989cc1ec74251b5623ae
Abdug'ani <xolmatjonovabdugani@gmail.com>
2025-02-19 15:18:36 +0500

    hi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
 
@@ -9,15 +9,15 @@ export const metadata: Metadata = {
 
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="en">
-        <body className="antialiased">
-        {children}
-        </body>
+            <body className="antialiased">
+                {children}
+            </body>
         </html>
     );
 }
