e7e16a7d2c12a343a9d995a6b2ddbe2d71419e53
Abdug'ani <xolmatjonovabdugani@gmail.com>
2025-02-21 22:21:11 +0500

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,20 @@ import Image from "next/image";
 
 export default function Home(){
     return (
-        <div>
+        <div className="w-screen h-screen flex justify-center items-center gap-8">
             <div>
-                <Image src="/fassco.png" width={203} height={200} alt="Fassco logo"></Image>
+                <Image src="/fassco.png" width={406} height={400} alt="Fassco logo"></Image>
+                <p>FASSCO</p>
+                <p>Панель управления складом</p>
+            </div>
+            <div>
+                <form action="">
+                    <p>имя</p>
+                    <input type="text" />
+                    <p>парол</p>
+                    <input type="password" />
+                    <button>Войти</button>
+                </form>
             </div>
         </div>
     );
